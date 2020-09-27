99355b40ef552364db2d618b8518cd008769ca19
Oleh.Kuryshko <Oleh.Kuryshko@gmail.com>
2020-09-27 19:58:14 +0300

    change output gcp-instance

--- gcp-instance/output.tf
+++ gcp-instance/output.tf
@@ -1,6 +1,6 @@
 output "private-ip" {
-  value = "${google_compute_instance.instance.network_interface.0.network_ip}"
+  value = "${google_compute_instance.instance[*].network_interface.0.network_ip}"
 }
 output "public-ip" {
-  value = "${google_compute_instance.instance.network_interface.0.access_config.0.nat_ip}"
+  value = "${google_compute_instance.instance[*].network_interface.0.access_config.0.nat_ip}"
 }
